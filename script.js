98d0785ff92f3db18aebddb1a66ea85394f28840
Harish Siddartha <69449561+Harish-Ranjith@users.noreply.github.com>
2026-02-25 17:37:22 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-let button = document.getElementById("btn");
-let title = document.getElementById("title");
-
-button.addEventListener("click", function () {
-    title.textContent = "You Clicked the Button!";
-    document.body.style.backgroundColor = "lightblue";
-});
